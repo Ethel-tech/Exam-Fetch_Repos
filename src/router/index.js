437d1d53c73689c6cf362ccb5162bf64c00b9061
Boma Sim-Jaja <56741091+Ethel-tech@users.noreply.github.com>
2023-03-18 23:27:55 +0100

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import ReposView from "../views/ReposView.vue";
+import SingleReposView from "../views/SingleReposView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/repos",
+      name: "repos",
+      component: ReposView,
+      children: [
+        {
+          path: "/repos/:id",
+          name: "singleRepos",
+          component: SingleReposView,
+          props: true,
+        },
+      ],
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "notFound",
+      component: NotFoundView,
+    },
+  ],
+});
+
+export default router;
